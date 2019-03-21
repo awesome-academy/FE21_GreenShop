2bb48e68133a94a4fb0b6f576808a171055929a7
thiennhat <thiennhat3004@gmail.com>
2019-03-21 21:53:13 +0700

    pull page home

--- gulpfile.js
+++ gulpfile.js
@@ -11,20 +11,19 @@ var pug = require('gulp-pug');
 var sass = require('gulp-sass');
 
 gulp.task('pug',function() { 
-  return gulp.src('./app/templates/pug/*.pug')  
+  return gulp.src('./app/templates/pug/pages/index.pug')  
     .pipe(pug({
      pretty : true//dòng này để export ra file html như bình thường
     }))   
-    .pipe(gulp.dest('./app/built/'))
+    .pipe(gulp.dest('./app/built'))
     .pipe(browserSync.stream());
 });
 
-  gulp.task('sass',function() {
-    // return gulp.src('./app/template/scss/*.scss')
-    return gulp.src(['./node_modules/bootstrap/scss/bootstrap.scss', './app/templates/scss/*.scss'])
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./app/built/css'))
-        .pipe(browserSync.stream());
+gulp.task('sass',function() {
+  return gulp.src('./app/templates/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))  
+    .pipe(gulp.dest('./app/built/css/'))
+    .pipe(browserSync.stream());
 });
 
 // add js
@@ -33,16 +32,25 @@ gulp.task('js', function() {
         .pipe(gulp.dest("./app/built/script"))
         .pipe(browserSync.stream());
 });
-// close js
 
 gulp.task('serve', ['pug','sass','js'], function() {
 
   browserSync.init({
-      server: "./app/built"
+      server: {
+        baseDir: "./app/built"
+      },
+      /*
+      *  server sẽ run file trong pages,nhưng nếu các file khác không nàm trong pages nó sẽ không nhận các file đó
+      *  nên file index phải nằm ngoài built
+      *   và đặc biệt đường dẫn chỉ tới file đó thôi chứ không được ghi tên file lun
+      */ 
   });
+  // browserSync.reload("./app/built/news.html");
+
   
   gulp.watch("app/templates/pug/**/*.pug", ['pug']);
   gulp.watch("app/templates/scss/**/*.scss", ['sass']);
+  gulp.watch("app/built/script/**/*.js", ['js']);
   gulp.watch("app/built/*.html").on('change', browserSync.reload);
 });
 
