acde91410c4e017ce2363c6399eee3967cfb76c7
thiennhat <thiennhat3004@gmail.com>
2019-03-24 17:44:57 +0700

    pull total 2

--- app/built/script/popup.js
+++ app/built/script/popup.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+ const popup = function() {
     $('#wrap-popup').css({
         top : '100px'
     })
@@ -7,9 +7,22 @@ $(document).ready(function() {
             display : 'none'
         })  
     })
-        // $('body:not("#wrap-popup")').on('click',function() {
-        //     $('#wrap-popup').css({
-        //         display : 'none'
-        //     })  
-        // })
-})
+ 
+ }      
+ 
+    
+    // cookie
+    window.addEventListener('DOMContentLoaded',function() {
+        
+        if(localStorage.getItem('name') != 'thiennhat') {
+            popup();
+            localStorage.setItem('name','thiennhat');
+        } else {
+            return true;
+        }
+
+        
+        
+        
+        
+    })
